715a303ac2986b0c83e7fa51b624f7360fcf8d1f
Poterasu Stefan <91740691+PoterasuStefan@users.noreply.github.com>
2024-05-20 01:45:31 +0300

    Delete Rojo directory

--- Rojo/Place_AutoRecovery_0/src/Workspace/SpawnLocation/MainGuiScript.server.lua
+++
@@ -1,7 +0,0 @@
-local Players = game:GetService("Players")
-local prompt = game.Workspace.SpawnLocation.ProximityPrompt
-
-prompt.Triggered:Connect(function(player)
-	local ScreenGui = Players[player.Name].PlayerGui.MainScreenGui
-	ScreenGui.Enabled = true
-end)
--- Rojo/Place_AutoRecovery_0/src/Workspace/carti/Script.server.lua
+++
@@ -1,7 +0,0 @@
-local Players = game:GetService("Players")
-local prompt = game.Workspace.carti.InvisiblePart.ProximityPromptBook
-
-prompt.Triggered:Connect(function(player)
-	local ComputerScienceLessonsGui = Players[player.Name].PlayerGui.ComputerScienceLessonsGui
-	ComputerScienceLessonsGui.Enabled = true
-end)
